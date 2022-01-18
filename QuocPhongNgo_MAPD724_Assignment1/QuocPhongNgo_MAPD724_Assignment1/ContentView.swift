83661254ae6df40da35c1c7c105ccb9f89b454c0
phongnq <phongnq2712@gmail.com>
2022-01-18 18:17:56 -0500

    added images and button

--- QuocPhongNgo_MAPD724_Assignment1/QuocPhongNgo_MAPD724_Assignment1/ContentView.swift
+++ QuocPhongNgo_MAPD724_Assignment1/QuocPhongNgo_MAPD724_Assignment1/ContentView.swift
@@ -1,9 +1,11 @@
-//
-//  ContentView.swift
-//  QuocPhongNgo_MAPD724_Assignment1
-//
-//  Created by Phong on 17/01/2022.
-//
+/**
+ * MAPD724 - Assignment 1
+ * File Name:    ContentView.swift
+ * Author:         Quoc Phong Ngo
+ * Student ID:   301148406
+ * Version:        1.0
+ * Date Modified:   January 18th, 2022
+ */
 
 import SwiftUI
 import CoreData
@@ -16,55 +18,104 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
 
+    private var symbols = ["apple", "cherry", "lemon"]
+    @State private var numbers = [0, 1, 2]
+    @State private var credits = 1000
+    @State private var betAmount: String = ""
+    private var jackPot = 5000
     var body: some View {
         ZStack {
             // Background
-            Rectangle()
-                .foregroundColor(Color(red: 255/255,
-                    green: 190/255, blue: 26/255))
-                .edgesIgnoringSafeArea(.all)
+//            Rectangle()
+//                .foregroundColor(Color(red: 255/255,
+//                    green: 190/255, blue: 26/255))
+//                .edgesIgnoringSafeArea(.all)
             
             Rectangle()
-                .foregroundColor(Color(red: 118/255,
-                    green: 64/255, blue: 29/255))
-                .rotationEffect(Angle(degrees: 135))
+                .foregroundColor(Color(red: 255/255,
+                    green: 250/255, blue: 171/255))
+//                .rotationEffect(Angle(degrees: 135))
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
+                Spacer()
                 HStack {
                     Text("Slot Machine")
                         .bold()
-                        .foregroundColor(Color(red: 255/255, green: 232/255, blue: 204/255))
+                        .foregroundColor(Color(red: 0/255, green: 106/255, blue: 98/255))
                 }.scaleEffect(2)
+                HStack {
+                    Image("slot-machine").resizable()
+                        .frame(width: 95.0, height: 95.0)
+                        .aspectRatio(1, contentMode: .fit)
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(20)
+                        .padding(.all, 10)
+                }
+                // Jackpot
+                Text("Jackpot: " + String(jackPot))
+                    .bold()
+                    .foregroundColor(.blue)
+                    .padding(.all, 10)
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(20)
                 // Credits counter
-                Text("Credits: 1000")
+                Text("Player Money: " + String(credits))
                     .foregroundColor(.black)
                     .padding(.all, 10)
                     .background(Color.white.opacity(0.8))
                     .cornerRadius(20)
+                HStack {
+                    Text("Bet Amount: ")
+                        .foregroundColor(.black)
+                        .padding(.all, 10)
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(20)
+                    TextField("Enter Bet", text: $betAmount)
+                        .foregroundColor(.red)
+                        .padding(.all, 10)
+                        .frame(width: 100, height: 35, alignment: .trailing)
+                }
+                
+                //Spacer()
                 // Cards
                 HStack {
+                    Spacer()
                     Image("apple").resizable()
-                        .frame(width: 75.0, height: 75.0)
+                        .frame(width: 95.0, height: 95.0)
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.8))
                         .cornerRadius(20)
-                        .padding(.all, 20)
+                        .padding(.all, 10)
                     
                     Image("cherry").resizable()
-                        .frame(width: 75.0, height: 75.0)
+                        .frame(width: 95.0, height: 95.0)
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.8))
                         .cornerRadius(20)
-                        .padding(.all, 20)
+                        .padding(.all, 10)
                     
                     Image("lemon").resizable()
-                        .frame(width: 75.0, height: 75.0)
+                        .frame(width: 95.0, height: 95.0)
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.8))
                         .cornerRadius(20)
-                        .padding(.all, 20)
+                        .padding(.all, 10)
+                    Spacer()
                 }
+                Spacer()
+                // Button
+                Button(action: {
+                    self.credits += 1
+                }) {
+                    Text("Spin")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.all, 15)
+                        .background(.blue)
+                        .cornerRadius(20)
+                }
+                Spacer()
             }
         }
 //        NavigationView {
