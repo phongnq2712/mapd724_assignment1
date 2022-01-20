3b7f520755de3f9adbedccb07270ad325e028399
phongnq <phongnq2712@gmail.com>
2022-01-20 00:23:50 -0500

    update layout design

--- QuocPhongNgo_MAPD724_Assignment1/QuocPhongNgo_MAPD724_Assignment1/ContentView.swift
+++ QuocPhongNgo_MAPD724_Assignment1/QuocPhongNgo_MAPD724_Assignment1/ContentView.swift
@@ -13,27 +13,17 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    private var symbols = ["apple", "cherry", "lemon"]
+    private var symbols = ["apple", "cherry", "lemon", "orange", "strawberry"]
     @State private var numbers = [0, 1, 2]
     @State private var credits = 1000
     @State private var betAmount: String = ""
+    @State private var isAvailableSpinButton = false
     private var jackPot = 5000
     var body: some View {
         ZStack {
-            // Background
-//            Rectangle()
-//                .foregroundColor(Color(red: 255/255,
-//                    green: 190/255, blue: 26/255))
-//                .edgesIgnoringSafeArea(.all)
-            
             Rectangle()
-                .foregroundColor(Color(red: 255/255,
-                    green: 250/255, blue: 171/255))
+                .foregroundColor(Color(red: 197/255,
+                    green: 231/255, blue: 255/255))
 //                .rotationEffect(Angle(degrees: 135))
                 .edgesIgnoringSafeArea(.all)
             
@@ -42,7 +32,7 @@ struct ContentView: View {
                 HStack {
                     Text("Slot Machine")
                         .bold()
-                        .foregroundColor(Color(red: 0/255, green: 106/255, blue: 98/255))
+                        .foregroundColor(Color(red: 137/255, green: 80/255, blue: 23/255))
                 }.scaleEffect(2)
                 HStack {
                     Image("slot-machine").resizable()
@@ -51,11 +41,11 @@ struct ContentView: View {
                         .background(Color.white.opacity(0.8))
                         .cornerRadius(20)
                         .padding(.all, 10)
-                }
+                }.padding(.bottom, 30)
                 // Jackpot
                 Text("Jackpot: " + String(jackPot))
                     .bold()
-                    .foregroundColor(.blue)
+                    .foregroundColor(Color(red: 137/255, green: 80/255, blue: 23/255))
                     .padding(.all, 10)
                     .background(Color.white.opacity(0.8))
                     .cornerRadius(20)
@@ -81,106 +71,83 @@ struct ContentView: View {
                 // Cards
                 HStack {
                     Spacer()
-                    Image("apple").resizable()
+                    Image(symbols[numbers[0]]).resizable()
                         .frame(width: 95.0, height: 95.0)
                         .aspectRatio(1, contentMode: .fit)
-                        .background(Color.white.opacity(0.8))
+                        .background(Color.black.opacity(0.8))
                         .cornerRadius(20)
                         .padding(.all, 10)
                     
-                    Image("cherry").resizable()
+                    Image(symbols[numbers[1]]).resizable()
                         .frame(width: 95.0, height: 95.0)
                         .aspectRatio(1, contentMode: .fit)
-                        .background(Color.white.opacity(0.8))
+                        .background(Color.black.opacity(0.8))
                         .cornerRadius(20)
                         .padding(.all, 10)
                     
-                    Image("lemon").resizable()
+                    Image(symbols[numbers[2]]).resizable()
                         .frame(width: 95.0, height: 95.0)
                         .aspectRatio(1, contentMode: .fit)
-                        .background(Color.white.opacity(0.8))
+                        .background(Color.black.opacity(0.8))
                         .cornerRadius(20)
                         .padding(.all, 10)
                     Spacer()
                 }
                 Spacer()
-                // Button
-                Button(action: {
-                    self.credits += 1
-                }) {
+                // Spin Button
+                Button(action: spinPressedButton) {
                     Text("Spin")
                         .bold()
                         .foregroundColor(.white)
                         .padding(.all, 15)
                         .background(.blue)
                         .cornerRadius(20)
+                }.padding(.bottom, 30).disabled(self.isAvailableSpinButton)
+                
+                HStack {
+                    // Reset Button
+                    Button(action: spinPressedButton) {
+                        Text("Reset")
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.all, 15)
+                            .background(.orange)
+                            .cornerRadius(20)
+                    }.padding(.bottom, 50)
+                    // Quit Button
+                    Button(action: spinPressedButton) {
+                        Text("Quit")
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.all, 15)
+                            .background(.orange)
+                            .cornerRadius(20)
+                    }.padding(.bottom, 50)
                 }
-                Spacer()
-            }
-        }
-//        NavigationView {
-//            List {
-//                ForEach(items) { item in
-//                    NavigationLink {
-//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//                    } label: {
-//                        Text(item.timestamp!, formatter: itemFormatter)
-//                    }
-//                }
-//                .onDelete(perform: deleteItems)
-//            }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
-//            Text("Select an item")
-//        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    
+    /**
+     * Event for pressing Spin button
+     */
+    private func spinPressedButton() {
+        // Check whether the user has enough money or not
+        let playerMoney = (betAmount as NSString).integerValue
+        if(playerMoney > self.credits) {
+            // grey out the Spin button
+            print("Not available")
+            self.isAvailableSpinButton = true
+            
+            return
+        } else {
+            self.isAvailableSpinButton = false
         }
+        self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+        self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+        self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
